19b91fff0f0cdb6a1b456c52b0924f532aa0f8ea
AKiroi <alexandr.kiroi@gmail.com>
2023-05-14 00:18:46 +0300

    feat: create setup lang to i18next

---
+++ src/18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+const lang = localStorage.getItem('i18nextLng') || 'en';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: lang,
+    debug: true,
+    detection: {
+      order: ['queryString', 'cookie'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
