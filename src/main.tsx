4e3da238fc2c9eaf7b019bce85a6f1d73fa96920
AKiroi <alexandr.kiroi@gmail.com>
2023-05-14 00:20:43 +0300

    feat: add i18next to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './18n';
 
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+        <App />
+    </Suspense>
   </React.StrictMode>
 );
