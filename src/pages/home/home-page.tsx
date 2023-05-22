7c2c31c0a96b0334f212d4a560c9e08a28a14759
AKiroi <alexandr.kiroi@gmail.com>
2023-05-23 02:34:50 +0300

    feat: add some styles and content to homePage

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,12 +1,29 @@
 import { FC } from 'react';
-import { useTypedTranslation } from '../../i18n/hooks';
+import { NavLink } from 'react-router-dom';
+import { routesMap } from '../../routes';
+//import { useTypedTranslation } from '../../i18n/hooks';
+//import { Loader } from '../../components/loader';
 
 export const HomePage: FC = () => {
-  const { t } = useTypedTranslation();
+  //const { t } = useTypedTranslation();
   return (
     <>
       <div className="homePage__container">
-        <h1>{t('homePageContent')}</h1>
+        {/*<h1>{t('homePageContent')}</h1>*/}
+        <div className="homePage__logo"></div>
+
+        <div className="homePage__button">
+          <span>Welcome to</span>
+          <NavLink to={routesMap.graphiql.path} className="homePage__link">
+            GraphQL!
+          </NavLink>
+        </div>
+        <div className="homePage__description">
+          GraphQL is a query language for APIs and a runtime for fulfilling those queries with your
+          existing data. GraphQL provides a complete and understandable description of the data in
+          your API, gives clients the power to ask for exactly what they need and nothing more,
+          makes it easier to evolve APIs over time, and enables powerful developer tools.
+        </div>
       </div>
     </>
   );
